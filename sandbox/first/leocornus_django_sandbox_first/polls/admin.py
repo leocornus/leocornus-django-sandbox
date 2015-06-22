4e3d46d28de8dfafb884879671a85e1c63224685
Sean Chen <sean.chen@leocorn.com>
2015-06-22 14:58:18 -0400

    elegant way to register models.

--- sandbox/first/leocornus_django_sandbox_first/polls/admin.py
+++ sandbox/first/leocornus_django_sandbox_first/polls/admin.py
@@ -4,5 +4,16 @@ from .models import Question
 from .models import Choice
 
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)

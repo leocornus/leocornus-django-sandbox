78ce6e8404367332a4f9fbf25bf842930909945d
Sean Chen <sean.chen@leocorn.com>
2015-06-23 08:58:37 -0400

    using template for the polls index page.

--- sandbox/first/leocornus_django_sandbox_first/polls/views.py
+++ sandbox/first/leocornus_django_sandbox_first/polls/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.template import loader
+
+from .models import Question
 
 # Create your views here.
 
 def index(request):
 
-    return HttpResponse("Hello, polls index...")
+    # get the lastest five questions.
+    questions = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('index.html')
+    context = RequestContext(request, {
+        'latest_question_list' : questions,
+    })
+    return HttpResponse(template.render(context))
 
